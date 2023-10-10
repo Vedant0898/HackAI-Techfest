86586394843cf0e8587f652d59f47877c9f9bfeb
gaganKUMMUR <gckummur@gmail.com>
2023-10-11 02:06:17 +0530

    pandas added

--- src/index.py
+++ src/index.py
@@ -1,6 +1,6 @@
 import os
 import json
-
+import pandas as pd
 import streamlit as st
 from streamlit.components.v1 import components
 
@@ -26,19 +26,25 @@ if "data.json" in os.listdir():
             st.title("Welcome {0}".format(name))
             st.write("{0}".format(email))
             st.write("Your base currency is set to {0}".format(base_currency))
-
+            dct ={}
             for i in target_currency:
                 mini = list(i.values())
                 # call api to get current exchange rates and display
                 r, y = get_exchange_rates(base_currency, list(i.keys()))
-                st.write(
-                    "{0} -> min {1}, max {2}, current value {3}".format(
-                        list(i.keys())[0],
-                        list(i.values())[0][0],
-                        list(i.values())[0][1],
-                        list(y.values())[0],
-                    )
-                )
+                dct['Currency'] = list(i.keys())[0]
+                dct['Min'] =  list(i.values())[0][0]
+                dct['Max'] =  list(i.values())[0][1]
+                dct['Current Value'] = list(y.values())[0]
+                # st.write(
+                #     "{0} -> min {1}, max {2}, current value {3}".format(
+                #         list(i.keys())[0],
+                #         list(i.values())[0][0],
+                #         list(i.values())[0][1],
+                #         list(y.values())[0],
+                #     )
+                # )
+            df = pd.DataFrame(dct)
+            st.dataframe(df,use_container_width=True)
 
 else:
     # Else ask user to create new preference
