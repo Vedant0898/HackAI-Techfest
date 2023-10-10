f5c53e3f48ec6cc522502d0b617842505583a16e
Vedant T <vedant.tamhane03@gmail.com>
2023-10-10 19:40:59 +0530

    comments and code cleanup

--- src/agents/user.py
+++ src/agents/user.py
@@ -1,56 +1,57 @@
+import os
+import json
+
+import dotenv
 from uagents import Agent, Context, Protocol
 from uagents.setup import fund_agent_if_low
-import json
 
 from messages.basic import ConvertRequest, ConvertResponse, Error, Notification
 
+# Load environment variables
+dotenv.load_dotenv()
 
+USER_AGENT_SEED = os.getenv("USER_AGENT_SEED", "user agent secret phrase")
+
+# Create user agent
 user_agent = Agent(
     name="user",
-    seed="user agent",
-    port=8002,
-    endpoint=["http://127.0.0.1:8002/submit"],
+    seed=USER_AGENT_SEED,
+    port=8003,
+    endpoint=["http://127.0.0.1:8003/submit"],
 )
 
+# Ensure the agent has enough funds
 fund_agent_if_low(user_agent.wallet.address())
 
-EXCHANGE_AGENT_ADDRESS = (
-    "agent1qt0ad7cync8z7fz35yrmcr0558734fa63jws9xuymmkrkhea67lqykkrfst"
-)
+# Address of exchange and notify agents
+EXCHANGE_AGENT_ADDRESS = os.getenv("EXCHANGE_AGENT_ADDRESS")
 
-NOTIFY_AGENT_ADDRESS = (
-    "agent1q2x94ysu3vxzm900g9d7j9j47egagfvd950j3rn09pljqls7ll7z7kyzhj5"
-)
+NOTIFY_AGENT_ADDRESS = os.getenv("NOTIFY_AGENT_ADDRESS")
 
-user_agent_protocol = Protocol("Convert")
-user_agent_protocol2 = Protocol("Notify")
+assert (
+    EXCHANGE_AGENT_ADDRESS is not None
+), "Need EXCHANGE_AGENT_ADDRESS, plese check README file"
 
-
-@user_agent.on_event("startup")
-async def initialize_storage(ctx: Context):
-    # try to get user's preferences from data.json
-    status = await update_internal_state(ctx, force=True)
-    if status:
-        return
-    # else set default values
-    ctx.storage.set("base", "INR")
-    default_targets = {
-        "USD": (1 / 85, 1 / 84),
-        "EUR": (1 / 90, 1 / 85),
-        "CAD": (1 / 58, 1 / 55),
-    }
-    ctx.storage.set("target", default_targets)
-    ctx.storage.set("name", "Username")
-    ctx.storage.set("email", "default.email@gmail.com")
+assert (
+    NOTIFY_AGENT_ADDRESS is not None
+), "Need NOTIFY_AGENT_ADDRESS, plese check README file"
 
 
-async def update_internal_state(ctx: Context, force=False):
+# Create a protocol for conversion requests and notifications
+user_agent_convert_protocol = Protocol("Convert")
+user_agent_notify_protocol = Protocol("Notify")
+
+
+# Function to read the user preferences from "data.json" file
+async def update_user_preference(ctx: Context, force=False):
     try:
         with open("data.json", "r") as file:
             data = json.load(file)
     except FileNotFoundError:
         ctx.logger.error("data.json not found. Skipping update.")
         return False
+    except Exception as e:
+        ctx.logger.error(f"Error in opening data.json : {e}")
 
     # check if data has been updated
     if data["hasChanged"] or force:
@@ -66,10 +67,30 @@ async def update_internal_state(ctx: Context, force=False):
     return False
 
 
-@user_agent_protocol.on_interval(600, messages=ConvertRequest)
+# Function to initialize the storage on startup
+@user_agent.on_event("startup")
+async def initialize_storage(ctx: Context):
+    # try to get user's preferences from data.json
+    status = await update_user_preference(ctx, force=True)
+    if status:
+        return
+    # else set default values
+    ctx.storage.set("base", "INR")
+    default_targets = {
+        "USD": (1 / 85, 1 / 84),
+        "EUR": (1 / 90, 1 / 85),
+        "CAD": (1 / 58, 1 / 55),
+    }
+    ctx.storage.set("target", default_targets)
+    ctx.storage.set("name", "Username")
+    ctx.storage.set("email", "default.email@gmail.com")
+
+
+# Timed function that will send a conversion request to exchange agent every 10 minutes
+@user_agent_convert_protocol.on_interval(600, messages=ConvertRequest)
 async def get_currency_conversion_rates(ctx: Context):
-    # update internal state
-    await update_internal_state(ctx)
+    # get updated user preferences
+    await update_user_preference(ctx)
 
     ctx.logger.info(f"Request sent to Exchange agent({EXCHANGE_AGENT_ADDRESS[:15]}...)")
     await ctx.send(
@@ -81,7 +102,8 @@ async def get_currency_conversion_rates(ctx: Context):
     )
 
 
-@user_agent_protocol.on_message(model=ConvertResponse)
+# Function to handle conversion response from exchange agent
+@user_agent_convert_protocol.on_message(model=ConvertResponse)
 async def handle_response(ctx: Context, sender: str, msg: ConvertResponse):
     ctx.logger.info(f"Received response from Exchange({sender[:15]}...)")
     thresholds = ctx.storage.get("target")
@@ -109,10 +131,12 @@ async def handle_response(ctx: Context, sender: str, msg: ConvertResponse):
         )
 
 
-@user_agent_protocol.on_message(model=Error)
+# Function to handle error from exchange agent
+@user_agent_convert_protocol.on_message(model=Error)
 async def handle_error(ctx: Context, sender: str, msg: Error):
     ctx.logger.error(f"Received Error from Exchange({sender[:15]}...): {msg.error}")
 
 
-user_agent.include(user_agent_protocol)
-user_agent.include(user_agent_protocol2)
+# include protocols with the agent
+user_agent.include(user_agent_convert_protocol)
+user_agent.include(user_agent_notify_protocol)
